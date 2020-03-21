66ab9a3d316d458d1d5118886f6e420dd847cfe2
Cactiw <Knyazev1514@gmail.com>
2020-03-21 14:25:07 +0300

    Added Sphere class

---
+++ Sphere.cpp
@@ -0,0 +1,17 @@
+//
+// Created by cactiw on 21.03.2020.
+//
+
+#include "Sphere.h"
+
+Vec3f Sphere::getCenter() {
+    return this->center;
+}
+
+float Sphere::getRadius() {
+    return this->radius;
+}
+
+Vec3f Sphere::getColor() {
+    return this->color;
+}
