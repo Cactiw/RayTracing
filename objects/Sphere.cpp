0d8e0df671721f3bb1444a45c54882faa438de54
Cactiw <Knyazev1514@gmail.com>
2020-03-23 23:59:38 +0300

    Sphere is now an Object child

---
+++ objects/Sphere.cpp
@@ -0,0 +1,47 @@
+//
+// Created by cactiw on 21.03.2020.
+//
+
+#include "Sphere.h"
+
+Vec3f Sphere::getCenter() const {
+    return this->center;
+}
+
+float Sphere::getRadius() const {
+    return this->radius;
+}
+
+Vec3f Sphere::getColor() const {
+    return this->color;
+}
+
+float dot(const Vec3f &point, const Vec3f &center) {
+    float norm = (point - center).norm();
+    return norm * norm;
+}
+
+float Sphere::check_intersect(const Ray &ray) const {
+    Vec3f oc = ray.getBeginPoint() - center;
+    float a = dot(ray.getTargetPoint(), ray.getTargetPoint());
+    float b = 2.0 * dot(oc, ray.getTargetPoint());
+    float c = dot(oc, oc) - radius*radius;
+    float discriminant = b*b - 4*a*c;
+    if(discriminant < 0){
+        return -1.0;
+    } else {
+        float numerator = -b - sqrt(discriminant);
+        if (numerator > 0.0) {
+            return numerator / (2.0 * a);
+        }
+
+        numerator = -b + sqrt(discriminant);
+        if (numerator > 0.0) {
+            return numerator / (2.0 * a);
+        }
+        else {
+            return -1;
+        }
+
+    }
+}
---
+++ objects/Sphere.h
@@ -0,0 +1,33 @@
+//
+// Created by cactiw on 21.03.2020.
+//
+
+#ifndef MACHINEGRAPHICS1TASK_SPHERE_H
+#define MACHINEGRAPHICS1TASK_SPHERE_H
+
+#include "../vector.cpp"
+#include "../classes/Ray.h"
+#include "Object.h"
+
+class Sphere : public Object{
+    Vec3f center;
+    float radius;
+
+    Vec3f color;
+
+public:
+
+    Sphere(Vec3f &ctr, Vec3f &clr, float radius) : Object(ctr, clr) {
+        this->radius = radius;
+    }
+
+    [[nodiscard]] Vec3f getCenter() const;
+    [[nodiscard]] float getRadius() const;
+    [[nodiscard]] Vec3f getColor() const;
+
+    [[nodiscard]] float check_intersect(const Ray &ray) const;
+
+};
+
+
+#endif //MACHINEGRAPHICS1TASK_SPHERE_H
