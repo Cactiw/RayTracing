a8f9d0f8a047ddf2a5e7311c23645b430eed807b
Cactiw <Knyazev1514@gmail.com>
2020-03-24 00:01:03 +0300

    Included libraries

--- vector.cpp
+++ vector.cpp
@@ -5,6 +5,8 @@
 #define VECTOR_3F_H
 
 #include <cassert>
+#include <cstddef>
+#include <cmath>
 
 template <size_t DIM, typename T> struct vec {
     vec() { for (size_t i=DIM; i--; data_[i] = T()); }
@@ -19,6 +21,14 @@ template <typename T> struct vec<3,T> {
     vec(T X, T Y, T Z) : x(X), y(Y), z(Z) {}
     T& operator[](const size_t i)       { assert(i<3); return i<=0 ? x : (1==i ? y : z); }
     const T& operator[](const size_t i) const { assert(i<3); return i<=0 ? x : (1==i ? y : z); }
+
+    vec<3, T> operator + (const vec<3, T> other) const {
+        return vec<3, T>(this->x + other->x, this->y + other->y, this->y + other->y);
+    }
+    vec<3, T> operator - (const vec<3, T> other) const {
+        return vec<3, T>(this->x - other->x, this->y - other->y, this->y - other->y);
+    }
+
     float norm() { return std::sqrt(x*x+y*y+z*z); }
     vec<3,T> & normalize(T l=1) { *this = (*this)*(l/norm()); return *this; }
     T x,y,z;
