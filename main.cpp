4e81d46420a84db7b326f167a3c7116e7f0cd41c
Cactiw <Knyazev1514@gmail.com>
2020-03-24 16:32:45 +0300

    Returned .ppm

--- main.cpp
+++ main.cpp
@@ -40,13 +40,16 @@ Color cast_ray(Ray &ray, std::vector<Object*> &objects) {
     return color;
 }
 
-std::vector<Color> generate_picture(std::vector<Object*> &objects) {
-    std::vector<Color> picture;
+std::vector<unsigned char> generate_picture(std::vector<Object*> &objects) {
+    std::vector<unsigned char> picture;
     for (size_t i = 0; i < PICTURE_WIDTH; ++i) {
         for (size_t j = 0; j < PICTURE_HEIGHT; ++j) {
             auto beginPoint = Vec3f(0, 0, 0), endPoint = Vec3f(i, j, 100);
             auto ray = Ray(beginPoint, endPoint);
-            picture.push_back(cast_ray(ray, objects));
+            auto color = cast_ray(ray, objects);
+            picture.push_back(color.getR());
+            picture.push_back(color.getG());
+            picture.push_back(color.getB());
         }
     }
     return picture;
@@ -57,15 +60,24 @@ Vec3f randomise_point() {
 }
 
 
-void save_picture(std::vector<Color> & picture) {
-    std::vector<unsigned char> finalPicture;
-    for (auto color: picture) {
-        finalPicture.push_back(color.getR());
-        finalPicture.push_back(color.getG());
-        finalPicture.push_back(color.getB());
+void save_picture(std::vector<unsigned char> & picture) {
+//    std::vector<unsigned char> finalPicture;
+//    for (auto color: picture) {
+//        finalPicture.push_back(color.getR());
+//        finalPicture.push_back(color.getG());
+//        finalPicture.push_back(color.getB());
+//    }
+//    stbi_write_jpg("./out.jpg", PICTURE_WIDTH, PICTURE_HEIGHT, CHANNELS_NUM,
+//            static_cast<void *>(finalPicture.data()), PICTURE_WIDTH * CHANNELS_NUM);
+//    stbi_write_jpg("./out.jpg", PICTURE_WIDTH, PICTURE_HEIGHT, 3, static_cast<void*>(picture.data()), 100);
+
+    std::ofstream ofs;
+    ofs.open("./out.ppm");
+    ofs << "P6\n" << PICTURE_WIDTH << " " << PICTURE_HEIGHT << "\n255\n";
+    for (auto c: picture) {
+        ofs << c;
     }
-    stbi_write_jpg("./out.jpg", PICTURE_WIDTH, PICTURE_HEIGHT, CHANNELS_NUM,
-            static_cast<void *>(finalPicture.data()), PICTURE_WIDTH * CHANNELS_NUM);
+    ofs.close();
 }
 
 void free_resources(std::vector<Object*> &objects) {
