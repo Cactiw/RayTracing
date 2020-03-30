eaa3be08582082f6346e068cbefe4c549b473629
Cactiw <Knyazev1514@gmail.com>
2020-03-30 14:47:17 +0300

    Added shadows

--- main.cpp
+++ main.cpp
@@ -27,13 +27,10 @@ enum {
 
 const Color BACKGROUND_COLOR = Color(BACKGROUND_COLOR_1, BACKGROUND_COLOR_2, BACKGROUND_COLOR_3);
 
-Color cast_ray(Ray &ray, std::vector<Object*> &objects, std::vector<Light*> &lights) {
+// Ищет первое пересечение луча с объектами сцены
+float find_first_intersect(const Ray &ray, const std::vector<Object*> &objects, Object* &hitObject,
+        Vec3f &hitPoint, Vec3f &normal, Color &color) {
     float min_dist = std::numeric_limits<float>::max();
-    Color color = BACKGROUND_COLOR;
-    float brightness = 0, glareBrightness = 0;
-    Vec3f hitPoint, normal;
-    bool hit = false;
-    Object* hitObject = nullptr;
     for (const auto& object: objects) {
         Vec3f tempHitPoint, tempNormal;
         float dist = object->check_intersect(ray, tempHitPoint, tempNormal);
@@ -44,19 +41,46 @@ Color cast_ray(Ray &ray, std::vector<Object*> &objects, std::vector<Light*> &lig
 //            std::cout << "Intersect " << ray.getTargetPoint().x << " " << ray.getTargetPoint().y << std::endl;
             min_dist = dist;
             color = object->getColor();
-            hit = true;  // Для отслеживания наличия пересечений - иначе свет не считаем
         }
     }
+    return hitObject != nullptr ? min_dist: -1;
+}
+
+
+Color cast_ray(Ray &ray, std::vector<Object*> &objects, std::vector<Light*> &lights) {
+    Color color = BACKGROUND_COLOR;
+    float brightness = 0, glareBrightness = 0;
+    Vec3f hitPoint, normal;
+    Object* hitObject = nullptr;
+    float min_dist = find_first_intersect(ray, objects, hitObject, hitPoint, normal, color);
+    bool hit = hitObject != nullptr;
+
 
     if (hit) {
+        Object* skipObject = nullptr;
+        Vec3f skip1, skip2;
+        Color skipColor = BACKGROUND_COLOR;
 //        Vec3f toViewer = hitPoint - ray.getBeginPoint();
         for (auto light: lights) {
-            Vec3f toLight = (light->getCenter() - hitPoint).normalize();
-            brightness += light->getBrightness() * std::max(0.f, toLight.dotProduct(normal));
-            Vec3f reflect = ((2 * (toLight.dotProduct(normal)) * normal) - toLight).normalize();
-            glareBrightness += std::max(0.f,
-                hitObject->getMaterial().getSpecular() * light->getBrightness() *
-                powf(reflect.dotProduct(normal), hitObject->getMaterial().getShininess()) * 50);
+            Vec3f toLight = light->getCenter() - hitPoint;
+            float lightDistance = toLight.norm();
+            toLight = toLight.normalize();
+            Vec3f originPoint = toLight.dotProduct(normal) > 0 ? hitPoint + normal * 1e-3 : hitPoint - normal * 1e-3;
+//            Vec3f originPoint = hitPoint + normal * 1e-3;
+            Vec3f lightPoint = light->getCenter();
+            Ray toLightRay(originPoint, lightPoint);
+            float intersectDistance = find_first_intersect(toLightRay, objects, skipObject, skip1, skip2, skipColor);
+            if (intersectDistance > 0 && intersectDistance < lightDistance) {
+                // Пересечение, ничего не прибавляем, точка в тени
+            } else {
+                brightness += light->getBrightness() * std::max(0.f, toLight.dotProduct(normal)); // Свет попадает?
+
+                Vec3f reflect = ((2 * (toLight.dotProduct(normal)) * normal) - toLight).normalize();
+                glareBrightness += std::max(0.f,
+                                            hitObject->getMaterial().getSpecular() * light->getBrightness() *
+                                            powf(reflect.dotProduct(normal), hitObject->getMaterial().getShininess()) *
+                                            50);
+            }
         }
     } else {
         brightness = 1;
@@ -96,15 +120,15 @@ void free_resources(std::vector<Object*> &objects) {
 }
 
 void add_objects(std::vector<Object*> &objects, std::vector<Light*> &lights) {
-    objects.push_back(new Sphere(Vec3f(PICTURE_WIDTH / 2. + 400, PICTURE_HEIGHT / 2. + 350,
+    objects.push_back(new Sphere(Vec3f(PICTURE_WIDTH / 2., PICTURE_HEIGHT / 2. + 350,
                                        PICTURE_WIDTH), Material(Color(30, 30, 180),
                                                1, 1.2, 40), 200));
-    objects.push_back(new Sphere(Vec3f(PICTURE_WIDTH / 2. + 250, PICTURE_HEIGHT / 2. - 250,
+    objects.push_back(new Sphere(Vec3f(PICTURE_WIDTH / 2., PICTURE_HEIGHT / 2. - 250,
                                        PICTURE_WIDTH), Material(Color(150, 0, 0),
                                                1, 1.2, 40), 150));
-    objects.push_back(new Sphere(Vec3f(PICTURE_WIDTH / 2. - 400, PICTURE_HEIGHT / 2.,
-                                       PICTURE_WIDTH + 300),Material(Color(0, 150, 50),
-                                               1, 1.2, 40), 100));
+    objects.push_back(new Sphere(Vec3f(PICTURE_WIDTH/ 2. + 300, 230,
+                                       PICTURE_WIDTH - 350),Material(Color(0, 150, 50),
+                                               1, 1.2, 40), 50));
 
     lights.push_back(new Light(Vec3f(PICTURE_WIDTH / 2., PICTURE_HEIGHT / 2., PICTURE_WIDTH - 1000),0.8));
     lights.push_back(new Light(Vec3f(PICTURE_WIDTH, 0, PICTURE_WIDTH - 1000),0.8));
