db54ae3f567f40919d6be363ca7b27f6f66f2bb6
Cactiw <Knyazev1514@gmail.com>
2020-04-01 00:46:15 +0300

    Compilation fixed

--- computations/physics.cpp
+++ computations/physics.cpp
@@ -1,9 +1,8 @@
 //
 // Created by cactiw on 31.03.2020.
 //
-
-#ifndef RAY_TRACING_PHYSICS
-#define RAY_TRACING_PHYSICS
+#ifndef MACHINEGRAPHICS1TASK_PHYSICS_H
+#define MACHINEGRAPHICS1TASK_PHYSICS_H
 
 #include <iostream>
 #include "vector.cpp"
@@ -18,20 +17,13 @@ Vec3f reflectVector(const Vec3f &v, const Vec3f &normal) {
 
 // Функия в чистом виде имплементирует закон Снеллиуса
 // v - падающий вектор, normal - нормаль к плоскости
-// refractive_index - индекс рефракции
-Vec3f refract(const Vec3f &v, const Vec3f &normal, const float &refractive_index) {
-    float cosi = -std::max(-1.f, std::min(1.f, v.dotProduct(normal)));
-    float etai = 1, etat = refractive_index;
-    Vec3f n = normal;
-    if (cosi < 0) {
-        // Луч остался внутри объекта. Инвертируем нормаль
-        cosi = -cosi;
-        std::swap(etai, etat);
-        n = -normal;
-    }
-    float eta = etai / etat;
-    float k = 1 - eta * eta * (1 - cosi * cosi);
-    return k < 0 ? Vec3f(0, 0, 0) : v * eta + n * (eta * cosi - sqrtf(k));
+// refractiveIndex - индекс рефракции
+Vec3f refract(const Vec3f &v, const Vec3f &normal, const float &refractiveIndex, const float &refractiveIndexAir = 1.f) {
+    float cosi = - std::max(-1.f, std::min(1.f, v.dotProduct(normal)));
+    if (cosi<0) return refract(v, -normal, refractiveIndexAir, refractiveIndex); // if the ray comes from the inside the object, swap the air and the media
+    float eta = refractiveIndexAir / refractiveIndex;
+    float k = 1 - eta*eta*(1 - cosi*cosi);
+    return k<0 ? Vec3f(1,1,1) : v*eta + normal*(eta*cosi - sqrtf(k));
 }
 
 #endif
