cdda6266b3be27fc75ac3e9dde2ce66bad768b5c
Cactiw <Knyazev1514@gmail.com>
2020-04-04 18:07:10 +0300

    Square computations fixed

--- objects/Square.cpp
+++ objects/Square.cpp
@@ -5,8 +5,29 @@
 #include <iostream>
 #include "Square.h"
 
-Square::Square(Vec3f a, Vec3f b, Vec3f c, Vec3f d): t1(a, b, c), t2(a, c, d) {
-
+Square::Square(Vec3f a, Vec3f b, Vec3f c, Vec3f d) : t1(a, b, c), t2(a, d, c) {
+    float ba = (b - a).norm(), ca = (c - a).norm(), da = (d - a).norm();
+    if (ba > ca) {
+        if (ba > da) {
+            // ba - диагональ
+            t1 = Triangle(a, b, c);
+            t2 = Triangle(a, b, d);
+        } else {
+            // da - диагональ
+            t1 = Triangle(a, d, b);
+            t2 = Triangle(a, d, c);
+        }
+    } else {
+        if (ca > da) {
+            // ca - диагональ
+            t1 = Triangle(a, c, b);
+            t2 = Triangle(a, c, d);
+        } else {
+            // da - диагональ
+            t1 = Triangle(a, d, b);
+            t2 = Triangle(a, d, c);
+        }
+    }
 }
 
 std::vector<Triangle> Square::toVector() const {
