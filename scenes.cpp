356315f0d477491f04838e8ea3e372a977f3e26c
Cactiw <Knyazev1514@gmail.com>
2020-04-05 21:31:56 +0300

    Added second scene

--- scenes.cpp
+++ scenes.cpp
@@ -24,11 +24,6 @@ void create_scene(std::vector<Object*> &objects, std::vector<Light*> &lights, Pi
                     YELLOW_FULL, 250));
 
 //            objects.push_back(new Figure("resources/cube.obj",
-//                                         Vec3f(PICTURE_WIDTH / 2. - 300, 250.,
-//                                                 PICTURE_WIDTH + 2000),
-//                                         1000,
-//                                         BLUE_FULL));
-//            objects.push_back(new Figure("resources/cube.obj",
 //                                         Vec3f(PICTURE_WIDTH / 2. - 700, PICTURE_HEIGHT - 250.,
 //                                                 PICTURE_WIDTH - 100),
 //                                         200,
@@ -51,6 +46,53 @@ void create_scene(std::vector<Object*> &objects, std::vector<Light*> &lights, Pi
 //                        1));
 
             break;
+        case 2:
+
+            objects.push_back(new Sphere(
+                    Vec3f(PICTURE_WIDTH / 2., PICTURE_HEIGHT / 2., PICTURE_WIDTH),
+                    MIRROR, 200));
+            objects.push_back(new Sphere(
+                    Vec3f(PICTURE_WIDTH/ 2. + 300, 230, PICTURE_WIDTH * 2 - 1350),
+                    GLASS, 150));
+
+            objects.push_back(new Figure("resources/duck.obj",
+                                         Vec3f(PICTURE_WIDTH / 2. + 150, PICTURE_HEIGHT - 500., PICTURE_WIDTH - 1000),
+                                         -50,
+                                         GLASS));
+
+            objects.push_back(new Figure("resources/cube.obj",
+                                         Vec3f(PICTURE_WIDTH - 650, 700., PICTURE_WIDTH - 1000),
+                                         -200,
+                                         GLASS));
+
+
+
+            objects.push_back(new Surface(
+                    Vec3f(1, 0, 0), Vec3f(3, 0, 0),
+                    Vec3f(2, 0, 1), WhITE_FULL
+            ));
+            objects.push_back(new Surface(
+                    Vec3f(1, PICTURE_HEIGHT, 0), Vec3f(3, PICTURE_HEIGHT, 0),
+                    Vec3f(2, PICTURE_HEIGHT, 1), WhITE_FULL
+            ));
+            objects.push_back(new Surface(
+                    Vec3f(0, 0, 0), Vec3f(0, 1, 0),
+                    Vec3f(0, 0, 1), GREEN_FULL
+            ));
+            objects.push_back(new Surface(
+                    Vec3f(0, 0, 5000), Vec3f(0, 1, 5000),
+                    Vec3f(1, 0, 5000), YELLOW_FULL
+            ));
+            objects.push_back(new Surface(
+                    Vec3f(PICTURE_WIDTH, 0, 0), Vec3f(PICTURE_WIDTH, 1, 0),
+                    Vec3f(PICTURE_WIDTH, 0, 1), RED_FULL
+            ));
+            objects.push_back(new Surface(
+                    Vec3f(0, 0, -1), Vec3f(0, 1, -1),
+                    Vec3f(1, 0, -1), YELLOW_FULL
+            ));
+
+            lights.push_back(new Light(Vec3f(PICTURE_WIDTH / 2., 100, PICTURE_WIDTH + 3000),1.6));
     }
     //    objects.push_back(new Sphere(
     //            Vec3f(PICTURE_WIDTH / 2. - 150, PICTURE_HEIGHT / 2. - 100, PICTURE_WIDTH - 450),
@@ -68,31 +110,6 @@ void create_scene(std::vector<Object*> &objects, std::vector<Light*> &lights, Pi
     //            Vec3f(PICTURE_WIDTH / 2. - 300, PICTURE_HEIGHT - 300., PICTURE_WIDTH),
     //            -100,
     //            YELLOW_FULL));
-
-    /*objects.push_back(new Surface(
-            Vec3f(1, 0, 0), Vec3f(3, 0, 0),
-            Vec3f(2, 0, 1), WhITE_FULL
-            ));
-    objects.push_back(new Surface(
-            Vec3f(1, PICTURE_HEIGHT, 0), Vec3f(3, PICTURE_HEIGHT, 0),
-            Vec3f(2, PICTURE_HEIGHT, 1), BLUE_FULL, WHITE_COLOR
-            ));
-    objects.push_back(new Surface(
-            Vec3f(0, 0, 0), Vec3f(0, 1, 0),
-            Vec3f(0, 0, 1), GREEN_FULL
-            ));
-    objects.push_back(new Surface(
-            Vec3f(0, 0, 5000), Vec3f(0, 1, 5000),
-            Vec3f(1, 0, 5000), YELLOW_FULL
-            ));
-    objects.push_back(new Surface(
-            Vec3f(PICTURE_WIDTH, 0, 0), Vec3f(PICTURE_WIDTH, 1, 0),
-            Vec3f(PICTURE_WIDTH, 0, 1), RED_FULL
-            ));
-    objects.push_back(new Surface(
-            Vec3f(0, 0, -1), Vec3f(0, 1, -1),
-            Vec3f(1, 0, -1), YELLOW_FULL
-    ));*/
 
 //    lights.push_back(new Light(Vec3f(PICTURE_WIDTH / 2., PICTURE_HEIGHT / 2., PICTURE_WIDTH - 1000),1.6));
 
