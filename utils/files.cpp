10d9ab9fa66728cf4e5816562e78e6c5848de9f0
Cactiw <Knyazev1514@gmail.com>
2020-04-05 17:35:26 +0300

    Files functions are now located in utils/files.h

---
+++ utils/files.cpp
@@ -0,0 +1,36 @@
+//
+// Created by cactiw on 05.04.2020.
+//
+
+#include <iostream>
+#include <vector>
+
+#include "files.h"
+#include "constants.cpp"
+
+
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+
+#include "../libs/stb/stb_image.h"
+#include "../libs/stb/stb_image_write.h"
+
+
+void save_picture(std::vector<Color> & picture) {
+    stbi_write_jpg("./out.jpg", PICTURE_WIDTH, PICTURE_HEIGHT, CHANNELS_NUM,
+                   static_cast<void*>(picture.data()), PICTURE_WIDTH * CHANNELS_NUM);
+}
+
+unsigned char * load_picture(const char * &path, int * width, int * height, int * channels) {
+    unsigned char * image = stbi_load(path, width, height, (int *) channels,
+            0);
+    if (!image) {
+        std::cerr << "Error while opening background image" << std::endl;
+        return nullptr;
+    }
+    return image;
+}
+
+void close_picture(unsigned char * picture) {
+    stbi_image_free(picture);
+}
---
+++ utils/files.h
@@ -0,0 +1,16 @@
+//
+// Created by cactiw on 05.04.2020.
+//
+
+#pragma once
+
+#ifndef RT_FILES_H
+#define RT_FILES_H
+
+#include "../classes/Color.h"
+
+void save_picture(std::vector<Color> & picture);
+unsigned char * load_picture(const char * &path, int * width, int * height, int * channels);
+void close_picture(unsigned char * picture);
+
+#endif //RT_FILES_H
